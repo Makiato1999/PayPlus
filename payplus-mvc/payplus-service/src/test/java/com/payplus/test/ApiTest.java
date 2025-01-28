c17d18f6234582a92379e77da72212b39ed0a837
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2025-01-28 03:52:46 -0500

    Update 1.28
    Update 1.28 除夕！

---
+++ payplus-mvc/payplus-service/src/test/java/com/payplus/test/ApiTest.java
@@ -0,0 +1,4 @@
+package com.payplus.test;
+
+public class ApiTest {
+}
---
+++ payplus-mvc/payplus-web/src/main/java/com/payplus/Application.java
@@ -0,0 +1,12 @@
+package com.payplus;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
